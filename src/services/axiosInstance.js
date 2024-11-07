f0cd723ca52cd4c969919f1330a84edcfebefef8
thaintd <114746768+thaintd@users.noreply.github.com>
2024-11-08 00:13:42 +0700

    bug cache

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -5,11 +5,24 @@ const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
     "Content-Encoding": "gzip",
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
     "Cache-Control": "no-cache, no-store, must-revalidate",
     Pragma: "no-cache",
     Expires: "0"
   }
 });
+
+// Sử dụng interceptor để thêm token vào mỗi request
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
